61bf776b06c2bf31a30b4fb53ba06d2e7aa06597
himik-nainwal <59734430+himik-nainwal@users.noreply.github.com>
2022-05-29 11:50:45 +0530

    Add files via upload

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,9 +1,10 @@
 import streamlit as st 
-import altair as alt
+import altair as alt   
 import plotly.express as px 
 import pandas as pd 
 import numpy as np 
 from datetime import datetime
+
 
 import joblib 
 pipe_lr = joblib.load(open("Models\Model_saved.pkl","rb"))
